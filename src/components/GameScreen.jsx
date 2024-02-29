a89ea97c1bea6f0c5331aa0e27a4498513ea47f0
lukblan <luk.blanko@gmail.com>
2024-02-29 17:37:44 -0300

    Player now can select pokemons

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -6,6 +6,16 @@ function GameScreen({ difficulty, onGameOver }) {
   const [playerLose, setPlayerLose] = useState(false)
   const [score, setScore] = useState(0)
   const [pokemons, setPokemons] = useState([])
+  const [selectedPokemons, setSelectedPokemons] = useState([])
+
+  const selectPokemon = (pokemonName) => () => {
+    if (selectedPokemons.includes(pokemonName)) {
+      setPlayerLose(true)
+    } else {
+      setSelectedPokemons([...selectedPokemons, pokemonName])
+      setScore(score + 1)
+    }
+  }
 
   useEffect(() => {
     const amountPokemonsToFetch = difficulty.cards
@@ -17,13 +27,17 @@ function GameScreen({ difficulty, onGameOver }) {
       {
         playerLose ?
           <>
+            <p>You Loose</p>
             <button onClick={() => setPlayerLose(false)}>Retry</button>
             <button onClick={onGameOver(null)}>Return</button>
           </>
           :
           <>
-            {pokemons.map(pokemon =>  <PokemonCard pokemon={pokemon}/> ) }
-            <button onClick={() => setPlayerLose(true)}>{difficulty.name} {pokemons.length}</button>
+            <p>Player score {score}</p>
+            {pokemons.map(pokemon =>  <PokemonCard key={pokemon.name} onClick={selectPokemon} pokemon={pokemon}/> ) }
+            <button onClick={() => setPlayerLose(true)}>
+              {difficulty.name}
+            </button>
           </>
       }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,8 @@
-function PokemonCard({pokemon}) {
+function PokemonCard({pokemon, onClick}) {
   const show = () => console.log(pokemon.name)
 
   return (
-    <div onClick={show}>
+    <div onClick={onClick(pokemon.name)}>
       <p>{pokemon.name}</p>
       <img src={pokemon.src}/>
     </div>
