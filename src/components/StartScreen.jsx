65d4306550c5f0b58cfa91330a0003318b5817dd
lukblan <luk.blanko@gmail.com>
2024-05-13 09:00:33 -0300

    Add rules on start screen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -2,14 +2,19 @@ import {DifficultyButton} from "./DifficultyButton.jsx";
 
 function StartScreen({difficulties, onDifficultyChange}) {
   return (
-    <ul className="difficulty-buttons-list">
-      {
-        difficulties.map(difficulty => <li><DifficultyButton key={difficulty.name}
-                                                         difficulty={difficulty}
-                                                         onClick={onDifficultyChange(difficulty)} /></li>)
-      }
-    </ul>
+    <>
+      <ul className="difficulty-buttons-list">
+        {
+          difficulties.map(difficulty => <li><DifficultyButton key={difficulty.name}
+                                                               difficulty={difficulty}
+                                                               onClick={onDifficultyChange(difficulty)}/></li>)
+        }
+      </ul>
+
+      <p className="rules score-board-text">Don't choose the same pokemon twice</p>
+    </>
+
   )
 }
 
-export { StartScreen}
+export {StartScreen}
